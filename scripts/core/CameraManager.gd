e37b4a5d4aa75b68f8063dbd039d9c213ee70e5f
John Barker <john@johnrbarker.com>
2026-02-28 22:42:44 +0000

    Set fixed overhead camera as default and active on start

--- scripts/core/CameraManager.gd
+++ scripts/core/CameraManager.gd
@@ -3,6 +3,10 @@ extends Node
 @export var rider_cam: Camera3D
 @export var overhead_cam: Camera3D
 
+func _ready() -> void:
+	if overhead_cam:
+		overhead_cam.make_current()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_camera"):
 		toggle_camera()
