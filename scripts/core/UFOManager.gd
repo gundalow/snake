6bc51e882a03d0d824a95653b07c60be909dfcaa
John Barker <john@johnrbarker.com>
2026-03-07 18:58:20 +0000

    Fix UFOManager signal and restore UID files after rebase

--- scripts/core/UFOManager.gd
+++ scripts/core/UFOManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal food_stolen
+
 @export var ufo_scene: PackedScene = preload("res://scenes/main/UFO.tscn")
 @export var spawn_interval: float = GameConstants.UFO_SPAWN_INTERVAL
 
@@ -23,12 +25,15 @@ func on_food_spawned(food: Node3D) -> void:
 func spawn_ufo(food: Node3D) -> void:
 	can_spawn = false
 	var ufo = ufo_scene.instantiate()
-	ufo.food_stolen.connect(get_parent()._on_food_stolen)
+	ufo.food_stolen.connect(_on_ufo_food_stolen)
 	# Spawn far away
 	var board_size = GameConstants.BOARD_SIZE
 	ufo.position = Vector3(board_size * 1.5, 5.0, board_size * 1.5)
 	if randf() > 0.5: ufo.position.x *= -1
 	if randf() > 0.5: ufo.position.z *= -1
 
-	get_parent().add_child(ufo)
+	add_child(ufo)
 	ufo.start_hunt(food)
+
+func _on_ufo_food_stolen() -> void:
+	food_stolen.emit()
